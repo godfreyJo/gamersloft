c06c483384a5349c12c257932427961e7d00a9e0
godfreyJo <godfreyoyugi@gmail.com>
2021-03-20 15:34:50 +0300

    working files

--- GLoft/GLoft/urls.py
+++ GLoft/GLoft/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
-from 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls'))
+     path('',include('mainLoft.urls')),
+    path('api/', include('api.urls'))
+
     
 ]
